f5bbd7088e5aa3e2604caf8d1aade81ec818aee5
tranhieu825 <60862609+tranhieu825@users.noreply.github.com>
2020-05-14 23:40:15 +0700

    Add files via upload

---
+++ database/migrations/2020_04_30_050926_create_tintuc_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTintucTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tintuc', function (Blueprint $table) {
+            $table->increments('id_tintuc');
+            $table->string('tieu_de');
+            $table->text('mota');
+            $table->integer('trang_thai');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tintuc');
+    }
+}
